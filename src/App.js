7e75bddc7eb7e43ce217f84f998120def4dad67f
austinroose <austin.roose1@gmail.com>
2021-01-12 17:13:21 +0200

    route change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
       return (
             <div>
-                <Router basename="/">
+                <Router>
                     <CustomLayout {...this.props}>
                         <BaseRouter />
                     </CustomLayout>
